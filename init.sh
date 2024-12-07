892c2384692012a5a73e464d65c9f9952ba1745f
Aleš <foxxcz@gmail.com>
2024-12-07 21:46:50 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# Export database connection details
+export PGPASSWORD="${DB_PASSWORD}"
+
 # Create Odoo role if it doesn't exist
-psql -v ON_ERROR_STOP=1 <<-EOSQL
+psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -v ON_ERROR_STOP=1 <<-EOSQL
     DO \$\$
     BEGIN
         IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$DB_ODOO_USER') THEN
